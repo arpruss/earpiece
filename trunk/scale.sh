ceab346b061c29a839b37a9ddcea77533fd6c71a
arpruss@gmail.com <arpruss@gmail.com@85baa61f-4be2-53ef-8073-d985eecc68a6>
2012-06-15 20:16:20 +0000

    1.02

--- trunk/scale.sh
+++ trunk/scale.sh
@@ -1,5 +1,5 @@
 cd res/drawable-hdpi
 for x in *.png ; do 
-  convert $x -scale 50% ../drawable-mdpi/$x
-  convert $x -scale 33.3333333% ../drawable-ldpi/$x
+  convert $x -scale 66.6666666666666% ../drawable-mdpi/$x
+  convert $x -scale 50% ../drawable-ldpi/$x
 done
